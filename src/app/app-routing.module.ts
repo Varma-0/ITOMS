bb3df8fac374f4e8df740761ea4031ca676677e1
varma <datlasairamvarma@gmail.com>
2024-06-09 19:48:49 +0530

    Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeFourComponent } from './home-four/home-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeFourComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreloaderComponent } from './layouts/preloader/preloader.component';
+import { HomeFourComponent } from './home-four/home-four.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreloaderComponent,
+    HomeFourComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
