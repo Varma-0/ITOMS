47b8c5bb7234d5b05fc3f12a00af5dd41cf93461
varma <datlasairamvarma@gmail.com>
2024-10-19 18:48:41 +0530

    landing

--- src/app/landing/dashboard/dashboard.component.ts
+++ src/app/landing/dashboard/dashboard.component.ts
@@ -7,13 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
   title = 'zero'
+  index = 0;
+  titles = ['zero','multi','firm','device','remote','custom']
   constructor() { }
 
   ngOnInit() {
+    // setInterval(() => {
+    //     this.index = this.index == 5 ? 0 : this.index+1;
+    //     this.title = this.titles[this.index];
+    //   }, 5000);
   }
 
   titleChange(name){
     this.title = name;
   }
+
+  previousSlide() {
+    this.index = this.index == 0 ? 5 : this.index -1;
+    this.title = this.titles[this.index];
+    }
+    nextSlide() {
+        this.index = this.index == 5 ? 0 : this.index+1;
+        this.title = this.titles[this.index];
+    }
 
 }
